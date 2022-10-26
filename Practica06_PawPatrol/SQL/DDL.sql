32d08230faafc183d63577ff3ada6209bed5e086
Rishi Yang <tikare@berkeley.edu>
2022-10-26 10:42:38 -0500

    empezó practica 06

---
+++ Practica06_PawPatrol/SQL/DDL.sql
@@ -0,0 +1,10 @@
+CREATE TABLE CLIENTE (
+  id_persona int PRIMARY KEY
+  numero_exterior int
+  cp int
+  calle varchar(32)
+  fecha_nacimiento date
+  nombre varchar(32)
+  apellido_paterno varchar(32)
+  apellido_materno varchar(32)
+)
