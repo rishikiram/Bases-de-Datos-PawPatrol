0e973d5b73abef51ebb9c5dd413c35d7efb16321
HotdogPoncho <hotdogponcho@ciencias.unam.mx>
2022-12-03 01:25:20 -0600

    Se completa clean

--- ProyectoFinal_PawPatrol/SQL/clean.sql
+++ ProyectoFinal_PawPatrol/SQL/clean.sql
@@ -17,3 +17,23 @@ DROP TABLE curso CASCADE;
 DROP TABLE horario_curso CASCADE;
 
 DROP TABLE impartir CASCADE;
+
+DROP TABLE sala CASCADE;
+
+--DROP TABLE sala_operaciones CASCADE;
+
+--DROP TABLE sala_capacitacion CASCADE;
+
+DROP TABLE asistencia CASCADE;
+
+DROP TABLE registro_asistencia CASCADE;
+
+DROP TABLE reservar CASCADE;
+
+DROP TABLE asignar CASCADE;
+
+DROP TABLE estacion CASCADE;
+
+DROP TABLE accesorio CASCADE;
+
+DROP TABLE cliente CASCADE;
