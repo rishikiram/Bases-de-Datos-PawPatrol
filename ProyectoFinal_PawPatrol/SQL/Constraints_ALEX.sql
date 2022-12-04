06ccdc1fceb0580f744d6f95381dfd9e8e8cb56e
Alex <alexndf@ciencias.unam.mx>
2022-12-03 20:58:38 -0600

    Constraint para evitar que empleados que no han aprobado los cursos necesarios laboren en una sala que lo requiere

--- ProyectoFinal_PawPatrol/SQL/Constraints_ALEX.sql
+++ ProyectoFinal_PawPatrol/SQL/Constraints_ALEX.sql
@@ -1,12 +1,25 @@
 -- ELIMINAR DROP ANTES DE ENTREGAR
-ALTER TABLE evaluar DROP CONSTRAINT calificacion_check_faltas;
+ALTER TABLE evaluar DROP CONSTRAINT evaluar_check_faltas;
 ALTER TABLE evaluar
-ADD CONSTRAINT calificacion_check_faltas CHECK (
-    calificacion_check_faltas_function(
+ADD CONSTRAINT evaluar_check_faltas CHECK (
+    evaluar_check_faltas_function(
       calificacion,
       id_empleado,
       id_curso,
       id_programa_curso,
       id_cliente
+    )
+  );
+
+-- ELIMINAR DROP ANTES DE ENTREGAR
+ALTER TABLE laborar_operaciones DROP CONSTRAINT laborar_operaciones_check_aprobacion;
+ALTER TABLE laborar_operaciones
+ADD CONSTRAINT laborar_operaciones_check_aprobacion CHECK (
+    laborar_operaciones_check_aprobacion_function(
+      id_reservacion_operaciones,
+      num_sala,
+      num_piso,
+      id_edificio,
+      id_empleado
     )
   );
