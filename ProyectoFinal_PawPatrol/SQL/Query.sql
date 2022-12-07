098d052d316b8c4b8fe96099f7bba185eb275a8b
Rishi Yang <tikare@berkeley.edu>
2022-12-07 11:26:21 -0600

    primer QUERY de rishi

--- ProyectoFinal_PawPatrol/SQL/Query.sql
+++ ProyectoFinal_PawPatrol/SQL/Query.sql
@@ -8,9 +8,9 @@ FROM sala_capacitacion s
         AND a.id_edificio = s.id_edificio
     )
 WHERE s.num_piso = 5
-    AND a.num_piso = 5
-	
-	
+    AND a.num_piso = 5;
+
+
 SELECT a.id_empleado,
     a.nombre,
     a.id_edificio,
@@ -21,15 +21,22 @@ FROM sala_capacitacion s
         AND a.id_edificio = s.id_edificio
     )
 WHERE s.num_piso = 5
-    AND a.num_piso = 5
-		
+    AND a.num_piso = 5;
+
 
 SELECT DISTINCT faltar.id_empleado, empleado.nombre, faltar.fecha
-FROM faltar 
+FROM faltar
 	INNER JOIN empleado ON(
 	faltar.id_empleado = empleado.id_empleado)
-WHERE fecha BETWEEN '2022-09-21' AND '2022-11-06'
+WHERE fecha BETWEEN '2022-09-21' AND '2022-11-06';
 
-	
-	
-	
+
+SELECT est.num_estacion, est.sistema_operativo, acc.tipo
+FROM estacion est
+	INNER JOIN accesorio acc ON(
+    est.num_estacion = acc.num_estacion AND
+    est.num_sala  = acc.num_sala AND
+    est.num_piso = acc.num_piso  AND
+    est.id_edificio = acc.id_edificio)
+WHERE est.sistema_operativo = 'Linux' AND
+  acc.tipo SIMILAR TO '%[Mm](OUSE|ouse)%';
