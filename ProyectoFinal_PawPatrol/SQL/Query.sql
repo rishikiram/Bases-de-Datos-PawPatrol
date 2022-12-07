e4eee51a497602093d74230c17ec527ce0da2709
Rishi Yang <tikare@berkeley.edu>
2022-12-07 12:26:55 -0600

    segundo query de rishi

--- ProyectoFinal_PawPatrol/SQL/Query.sql
+++ ProyectoFinal_PawPatrol/SQL/Query.sql
@@ -30,8 +30,8 @@ FROM faltar
 	faltar.id_empleado = empleado.id_empleado)
 WHERE fecha BETWEEN '2022-09-21' AND '2022-11-06';
 
-
-SELECT est.num_estacion, est.sistema_operativo, acc.tipo
+--Todos los estaciones con sistema operativo 'Linux' y con un mouse
+SELECT est.num_estacion, est.num_sala, est.num_piso, est.id_edificio, est.sistema_operativo, acc.tipo
 FROM estacion est
 	INNER JOIN accesorio acc ON(
     est.num_estacion = acc.num_estacion AND
@@ -40,3 +40,11 @@ FROM estacion est
     est.id_edificio = acc.id_edificio)
 WHERE est.sistema_operativo = 'Linux' AND
   acc.tipo SIMILAR TO '%[Mm](OUSE|ouse)%';
+
+--numero de dias trabajado en operaciones de empleado numero 13 en 2022
+SELECT COUNT(fro.fecha) num_dias
+FROM empleado emp
+  INNER JOIN laborar_operaciones lo ON emp.id_empleado = lo.id_empleado
+  INNER JOIN fecha_reservacion_operaciones fro ON fro.id_reservacion_operaciones = lo.id_reservacion_operaciones
+WHERE emp.id_empleado = 13 and fro.fecha BETWEEN '2022-01-01' AND '2022-12-31'
+GROUP BY emp.id_empleado;
