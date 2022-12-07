aa2ad154ce9b32a2b2343b3207db4b942e43f180
Rishi Yang <tikare@berkeley.edu>
2022-12-07 13:35:38 -0600

    olvidé clean_triggers.sql

--- ProyectoFinal_PawPatrol/SQL/clean_Triggers.sql
+++ ProyectoFinal_PawPatrol/SQL/clean_Triggers.sql
@@ -1,4 +1,3 @@
-DROP TRIGGER  IF EXISTS  num_de_pisos ON piso CASCADE;
-DROP TRIGGER  IF EXISTS  num_de_salas ON sala CASCADE;
-DROP TRIGGER  IF EXISTS  num_de_salas_capacitacion ON sala_capacitacion CASCADE;
-DROP TRIGGER  IF EXISTS  num_de_salas_operacion ON sala_operacion CASCADE;
+DROP TRIGGER IF EXISTS num_de_pisos ON piso CASCADE;
+DROP TRIGGER IF EXISTS num_faltas_update ON evaluar CASCADE;
+DROP TRIGGER IF EXISTS num_faltas_insert ON evaluar CASCADE;
